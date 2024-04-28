ebfd222ce1b205dce6605494a02207ab2ce01298
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-28 20:19:57 +0300

    probably last fix

--- src/app/needs/modal.js
+++ src/app/needs/modal.js
@@ -1,88 +1,71 @@
 import React, { useState } from 'react';
 import styles from './modal.module.css';
 import { registerFundraising, registerHumanitarianAid } from "@/util/api";
-import { router } from "next/client";
 
 const Modal = ({ handleCloseModal }) => {
     const [selectedOption1, setSelectedOption1] = useState('');
-    const [selectedOption2, setSelectedOption2] = useState('');
     const [isDropdownOpen1, setIsDropdownOpen1] = useState(false);
-    const [isDropdownOpen2, setIsDropdownOpen2] = useState(false);
-
-
 
     const [formDataFundraising, setFormDataFundraising] = useState({
         description: "",
-        donationUrl: "",
         forWhom: "",
         goalName: "",
         imageUrl: "",
-        moneyGoal: 0,
+        moneyGoal: "",
         needyThing: ""
-
     });
+
     const [formDataHumanitarianAid, setFormDataHumanitarianAid] = useState({
         city: "",
         description: "",
-        imageUrl: "",
         needName: ""
-
     });
+
     const handleInputHumanChange = event => {
         const { name, value } = event.target;
-        console.log({ name, value })
-        console.log(formDataHumanitarianAid)
         setFormDataHumanitarianAid(prevState => ({
             ...prevState,
             [name]: value
         }));
     };
+
     const handleInputChange = event => {
         const { name, value } = event.target;
-        console.log({ name, value })
-        console.log(formDataFundraising)
         setFormDataFundraising(prevState => ({
             ...prevState,
             [name]: value
         }));
     };
+
     const handleFundraising = async (event) => {
-
         event.preventDefault();
-
         try {
-            const token = localStorage.getItem('accessToken')
-            console.log(token)
+            const token = localStorage.getItem('accessToken');
             const responseData = await registerFundraising(formDataFundraising, token);
             console.log('Response data:', responseData);
+            handleCloseModal();
+            window.location.reload();
         } catch (error) {
             console.error('Error handling fundraising:', error);
-        }
-    }
-    const handleHumanitarianAid = async (event) => {
-        console.log('human')
-        event.preventDefault();
-
-        try {
-            const token = localStorage.getItem('accessToken')
-            console.log(token)
-            const responseData = await registerHumanitarianAid(formDataFundraising, token);
-            console.log('Response data:', responseData);
-        } catch (error) {
-            console.error('Error handling fundraising:', error);
-        }
-    }
-    const handleOptionClick1 = (option) => {
-        setSelectedOption1(option);
-        setIsDropdownOpen1(false);
-        if (option === 'Гуманітарна допомога') {
-            setIsDropdownOpen2(false);
         }
     };
 
-    const handleOptionClick2 = (option) => {
-        setSelectedOption2(option);
-        setIsDropdownOpen2(false);
+    const handleHumanitarianAid = async (event) => {
+        event.preventDefault();
+        try {
+            const token = localStorage.getItem('accessToken');
+            const responseData = await registerHumanitarianAid(formDataFundraising, token);
+            console.log('Response data:', responseData);
+            handleCloseModal();
+            window.location.reload();
+        } catch (error) {
+            console.error('Error handling fundraising:', error);
+        }
+    };
+
+    const handleOptionClick1 = (option) => {
+        setSelectedOption1(option);
+        setIsDropdownOpen1(false);
     };
 
     return (
@@ -185,43 +168,6 @@ const Modal = ({ handleCloseModal }) => {
                         placeholder="Опис"
                         className={styles.input}
                     />
-                </div>
-            )}
-            {selectedOption1 === 'Збір' && (
-                <div className={styles.selectWrapper}>
-                    <div
-                        className={styles.selectedOption}
-                        onClick={() => setIsDropdownOpen2(!isDropdownOpen2)}
-                    >
-                        {selectedOption2 || 'Реквізити'}
-                        <img src="/arrow.svg" alt="Arrow" className={styles.arrowIcon} />
-                    </div>
-                    {isDropdownOpen2 && (
-                        <div className={styles.inputContainer3}>
-                            <input
-                                type="text"
-                                value={formDataFundraising.moneyGoal}
-                                onChange={handleInputChange}
-                                placeholder="UAH"
-                                className={styles.input}
-                            />
-                            <input
-                                type="text"
-                                value={formDataFundraising.donationUrl}
-                                onChange={handleInputChange}
-                                placeholder="Номер картки"
-                                className={styles.input}
-                            />
-                            <input
-                                type="text"
-                                name='donationUrl'
-                                value={formDataFundraising.donationUrl}
-                                onChange={handleInputChange}
-                                placeholder="Посилання на монобанк"
-                                className={styles.input}
-                            />
-                        </div>
-                    )}
                 </div>
             )}
             <button
